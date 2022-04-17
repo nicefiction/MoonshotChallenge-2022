2e529bf4126baca2af44681c1acc4ae697729249
Olivier Van hamme <nicefiction@gmail.com>
2022-04-17 21:23:44 +0200

    Initial Commit

---
+++ MoonshotChallenge/MoonshotChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotChallengeApp.swift
+//  MoonshotChallenge
+//
+//  Created by Olivier Van hamme on 17/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
